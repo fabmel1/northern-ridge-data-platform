5e22079df15b0913e3d57b7280765ed13185f33f
Fabian Melo <fabmel1@users.noreply.github.com>
2026-08-06 08:22:16 -0500

    Adding categories to catalog

--- SQL Scripts/Test_stage.sql
+++ SQL Scripts/Test_stage.sql
@@ -3,3 +3,15 @@ USE DATABASE NORTHERN_RIDGE_DB;
 USE SCHEMA BRONZE;
 
 LIST @STG_LANDING_ZONE;
+
+--Monitor tasks from last 2 hours
+SELECT
+    name,
+    state,
+    completed_time,
+    error_message
+FROM TABLE(INFORMATION_SCHEMA.TASK_HISTORY(
+    task_name => 'TSK_AUTO_INGEST_BRONZE',
+    scheduled_time_range_start => DATEADD('hour', -2, CURRENT_TIMESTAMP())
+))
+ORDER BY completed_time DESC;
